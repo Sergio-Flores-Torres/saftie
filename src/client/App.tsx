a8ed4061baadffd2a83e2ec27326c561a10e9b36
Sergio Flores <sergio@saft.industries>
2024-10-19 21:30:06 -0600

    full blink code

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -15,7 +15,7 @@ export default function App() {
   const { data: user } = useAuth();
 
   const shouldDisplayAppNavBar = useMemo(() => {
-    return location.pathname !== '/' && location.pathname !== '/login' && location.pathname !== '/signup';
+    return location.pathname !== '/' && location.pathname !== '/login' && location.pathname !== '/signup' && location.pathname !== '/blink';
   }, [location]);
 
   const isAdminDashboard = useMemo(() => {
--- src/solana/BlinkPage.tsx
+++ src/solana/BlinkPage.tsx
@@ -8,34 +8,65 @@ import {
   createSaftie,
 } from 'wasp/client/operations';
 
-import { useState, useMemo } from 'react';
-import { CgSpinner } from 'react-icons/cg';
-import { TiDelete, TiClipboard } from 'react-icons/ti';
 import { cn } from '../client/cn';
+
+import '@dialectlabs/blinks/index.css';
+import { useState, useEffect, useMemo } from 'react';
+import { Action, Blink, ActionsRegistry, useAction, } from "@dialectlabs/blinks";
+//import { useAction } from '@dialectlabs/blinks/react';
+import { useActionSolanaWalletAdapter } from '@dialectlabs/blinks/hooks/solana';
+import { UnsafeBurnerWalletAdapter } from '@solana/wallet-adapter-wallets';
+
+import {
+	ConnectionProvider,
+	WalletProvider,
+  } from '@solana/wallet-adapter-react';
+import { WalletModalProvider } from '@solana/wallet-adapter-react-ui';
 
 export default function BlinkPage() {
 
-  return (
-    <div className='py-10 lg:mt-10'>
-      <div className='mx-auto max-w-7xl px-6 lg:px-8'>
-        <div className='mx-auto max-w-4xl text-center'>
-          <h2 className='mt-2 text-4xl font-bold tracking-tight text-gray-900 sm:text-5xl dark:text-white'>
-            <span className='text-yellow-500'>Saftie</span> Manager
-          </h2>
-        </div>
-        <p className='mx-auto mt-6 max-w-2xl text-center text-lg leading-8 text-gray-600 dark:text-white'>
-          Create/Delete your Safties below. Each Saftie will need a destination Solana address and an amount in 
-          USDC; with that, you'll get a Solana Blink that you can share with your donors on the Internet!
-        </p>
-        {/* begin AI-powered Todo List */}
-        <div className='my-8 border rounded-3xl border-gray-900/10 dark:border-gray-100/10'>
-          <div className='sm:w-[90%] md:w-[70%] lg:w-[60%] py-10 px-6 mx-auto my-8 space-y-10'>
-            
+	//const actionApiUrl = 'solana-action:http://saftie.saft.industries/solana-actions/';
+	const actionApiUrl = 'solana-action:https://dial.to/api/donate';
+	const endpoint = import.meta.env.REACT_APP_PUBLIC_SOLANA_RPC_URL;
 
-          </div>
-        </div>
-        {/* end AI-powered Todo List */}
-      </div>
-    </div>
+	const { adapter } = useActionSolanaWalletAdapter(
+		endpoint,
+	);
+
+	const { action, isLoading } = useAction({
+		url: actionApiUrl,
+		adapter,
+	});
+
+    const wallets = useMemo(
+        () => [
+            /**
+             * Wallets that implement either of these standards will be available automatically.
+             *
+             *   - Solana Mobile Stack Mobile Wallet Adapter Protocol
+             *     (https://github.com/solana-mobile/mobile-wallet-adapter)
+             *   - Solana Wallet Standard
+             *     (https://github.com/anza-xyz/wallet-standard)
+             *
+             * If you wish to support a wallet that supports neither of those standards,
+             * instantiate its legacy wallet adapter here. Common legacy adapters can be found
+             * in the npm package `@solana/wallet-adapter-wallets`.
+             */
+            new UnsafeBurnerWalletAdapter(),
+        ],
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+        [endpoint]
+    );
+
+  return (
+    <ConnectionProvider endpoint={endpoint}>
+      <WalletProvider wallets={wallets} autoConnect>
+        <WalletModalProvider>
+			{action ?
+			<Blink action={action} websiteText={new URL(actionApiUrl).hostname} />
+			: null}
+		</WalletModalProvider>
+      </WalletProvider>
+    </ConnectionProvider>
   );
 }
